8b2c3a67ced654e423f71d2dcdf34d094493aab6
unknown <elbert.herry11@gmail.com>
2024-01-08 22:33:12 +0700

    next:Edit & Delete Post

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// User related routes
+Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
+
+// Blog post related routes
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('anuan');
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
+Route::get('/post/{post}',[PostController::class,'viewSinglePost']);
+
+// Profile related routes
+Route::get('/profile/{user:username}', [UserController::class, 'viewProfile']);
+
+
